94c4842f633a7043554dcbd467b4aa0216390052
azamd <47691398+azamd@users.noreply.github.com>
2019-03-25 13:02:56 +0100

    Add files via upload
    Translation de l'image du fond (Background) par rapport a la camera.

---
+++ MedAzizAmdouni/Scrolling 1/scroll.h
@@ -0,0 +1,10 @@
+#ifndef JEU_H_INCLUDED
+#define JEU_H_INCLUDED
+#include <SDL/SDL.h>
+
+
+void scrolling(SDL_Surface *screen,SDL_Surface *imgfond ,int running,SDL_Rect posfond);
+
+#endif // JEU_H_INCLUDED
+
+

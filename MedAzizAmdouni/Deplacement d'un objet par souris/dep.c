fb358d3f210b26a18282817a60624b6b379e2ed2
azamd <47691398+azamd@users.noreply.github.com>
2019-03-25 13:14:19 +0100

    Update dep.c

--- MedAzizAmdouni/Deplacement d'un objet par souris/dep.c
+++ MedAzizAmdouni/Deplacement d'un objet par souris/dep.c
@@ -18,33 +18,33 @@ void translation(SDL_Surface *screen,SDL_Surface *img ,int running,SDL_Rect pos)
          running = 0;
          break;
 
-         case SDL_MOUSEBUTTONUP:
-             if(event.button.button==SDL_BUTTON_LEFT)
+         case SDL_MOUSEBUTTONUP: // cliquer la souris
+             if(event.button.button==SDL_BUTTON_LEFT) // bouton gauche de la souris
               {
               pos.x = event.button.x-513;
              pos.y = event.button.y-400;
 
       SDL_BlitSurface(img,NULL,screen,&pos);
               }
-              else if (event.button.button==SDL_BUTTON_RIGHT)
+              else if (event.button.button==SDL_BUTTON_RIGHT) // bouton droit de la souris
 
               running=0;
 
               break ;
-         case SDL_ACTIVEEVENT :
-                   if ((event.active.state & SDL_APPMOUSEFOCUS) == SDL_APPMOUSEFOCUS)
+         case SDL_ACTIVEEVENT : //gérer la fenetre
+                   if ((event.active.state & SDL_APPMOUSEFOCUS) == SDL_APPMOUSEFOCUS) //verification de l'etat de la souris
 {
-      if (event.active.gain == 0)  
+      if (event.active.gain == 0)  // cas n°1 : le curseur hors la fenetre.
         {
          pos.x ++ ;
-         pos.y++ ;
-SDL_BlitSurface(img,NULL,screen,&pos);
+         pos.y ++ ;
+         SDL_BlitSurface(img,NULL,screen,&pos);
         }
-      else if (event.active.gain == 1)  
+      else if (event.active.gain == 1) // cas n°2 : le curseur dans la fenetre. 
         {
-                   pos.x ++ ;
-                   pos.y ++ ;
-SDL_BlitSurface(img,NULL,screen,&pos);
+              pos.x ++ ;
+              pos.y ++ ;
+              SDL_BlitSurface(img,NULL,screen,&pos);
 
             break ;
         }

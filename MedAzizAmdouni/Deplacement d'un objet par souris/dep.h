18afa3db9f21054febf1b25461a5a67c1bb95f39
azamd <47691398+azamd@users.noreply.github.com>
2019-03-25 17:54:58 +0100

    Update dep.h

--- MedAzizAmdouni/Deplacement d'un objet par souris/dep.h
+++ MedAzizAmdouni/Deplacement d'un objet par souris/dep.h
@@ -2,6 +2,6 @@
 #define JEU_H_INCLUDED
 #include <SDL/SDL.h>
 
-void deplacement(SDL_Surface *screen,SDL_Surface *img ,int running,SDL_Rect pos);
+void translation(SDL_Surface *screen,SDL_Surface *img ,int running,SDL_Rect pos);
 
 #endif // JEU_H_INCLUDED

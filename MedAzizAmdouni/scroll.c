6d38eb9785a0bb47a7f87218cc4683213fb6c62a
azamd <47691398+azamd@users.noreply.github.com>
2019-03-25 11:16:36 +0100

    Update scroll.c

--- MedAzizAmdouni/scroll.c
+++ MedAzizAmdouni/scroll.c
@@ -6,28 +6,28 @@
 
 void scrolling(SDL_Surface *screen,SDL_Surface *imgfond ,int running,SDL_Rect posfond)
      {
-    posfond.x = 0;
+    posfond.x = 0;   //définition des coordonnés (sure l'axe x,y)
     posfond.y = 0;
-    posfond.h =500;
-    posfond.w =1920;
+    posfond.h =500;  //Hauteur h
+    posfond.w =1920  //Largeur w
 
     
-    imgfond = IMG_Load("backg.png");
+    imgfond = IMG_Load("backg.png");  //chargement de l'image du fond (background)
    
-  SDL_BlitSurface(imgfond,&posfond,screen, NULL);
+  SDL_BlitSurface(imgfond,&posfond,screen, NULL);  //ajuster l'image du fond dans la position predefinie
               
-    SDL_Flip(screen);
+    SDL_Flip(screen); //mise a jour
     
  
     SDL_Event event;
     SDL_EnableKeyRepeat(10, 10);
  
-    while (running)
+    while (running) // running==1
     {
         SDL_WaitEvent(&event);
         switch(event.type)
         {
-            case SDL_QUIT:
+            case SDL_QUIT: //appui sur le bouton de fermeture (x)
                 running= 0;
                 break;
             case SDL_KEYDOWN:
@@ -40,7 +40,7 @@ void scrolling(SDL_Surface *screen,SDL_Surface *imgfond ,int running,SDL_Rect po
             	
                 if  ( posfond.x<=5760)
        
-            posfond.x += 10;
+            posfond.x += 10; // translation vers gauche du background sur l'axe x 
      
             
        
@@ -54,7 +54,7 @@ void scrolling(SDL_Surface *screen,SDL_Surface *imgfond ,int running,SDL_Rect po
             case SDLK_LEFT: // appui sur la flèche gauche
             	if ( posfond.x>=0)
         
-            posfond.x -= 10;
+            posfond.x -= 10; // translation vers droite du background sur l'axe x
      
          
                 SDL_BlitSurface(imgfond,&posfond,screen, NULL);
@@ -62,16 +62,9 @@ void scrolling(SDL_Surface *screen,SDL_Surface *imgfond ,int running,SDL_Rect po
                 
     SDL_Flip(screen);
 
-                break;
-              
-
-        }
-
-        break;    
-        }
-
-       
-        
+                break;  }
+		break;  }
+   
     SDL_Flip(screen);
     }
 }

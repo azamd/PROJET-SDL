06806f9e099604b0be73dee2599b5afa0c4322b6
rahimbm <47696010+rahimbm@users.noreply.github.com>
2019-04-01 22:07:37 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,59 +1,34 @@
-#include <stdio.h>
-#include "SDL/SDL.h"
-#include "SDL/SDL_image.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<SDL/SDL.h>
+#include <SDL/SDL_image.h> 
+#include "deplacment.h"
 
-int initobjet 
+
+
+
+int main ()
 {
-	SDL_Surface *objet;
-	
-	SDL_Surface *screen;
-	SDL_Event event;
-	SDL_Rect positionobjet,positionscreen;
+int continuer=1 ;
+
+SDL_Surface *ecran =NULL;
+SDL_Surface *imageFond=NULL ;
+SDL_Surface *personage=NULL;
+SDL_Rect positionDuFond ;
+SDL_Rect positionPerso ;
 
 
-	int objet_x = 20;
-	int objet_y = 5;
-	
+SDL_Init (SDL_INIT_VIDEO);
 
+ecran= SDL_SetVideoMode(667,250,32,SDL_HWSURFACE|SDL_DOUBLEBUF);
+SDL_WM_SetCaption("Deplacer un personage",NULL);
 
+deplacment (ecran,imageFond,personage,positionDuFond,positionPerso ,continuer);
 
-	if(SDL_Init(SDL_INIT_VIDEO)!=0)
-	{
-		printf("Unable to initialize SDL:%s\n",SDL_GetError());
-		return 1;
-	}
-	screen=SDL_SetVideoMode(660,260,32,SDL_HWSURFACE|SDL_DOUBLEBUF);
-	if(screen==NULL)
-	{
-		printf("unable to set video mode :%s\n",SDL_GetError());
-		return 1;
-	}
-	
-	
-
-
-	objet=IMG_Load("objet.png");	
-	if(image==NULL)
-	{
-		printf("unable to load bitamp: %s",SDL_GetError());
-		return 1;
-	}
-	
-	
-
-	
-
-	positionobjet1.x = ecran_x;
-	positionobjet1.y = ecran_y;
-	
-
-
-	SDL_BlitSurface(image, NULL, screen, &positionscreen);
-	
-	SDL_BlitSurface(objet, NULL, screen, &positionobjet);
-
-	
-	
-	
-
+SDL_FreeSurface(personage);
+SDL_FreeSurface(imageFond);
+SDL_Quit();
+return EXIT_SUCCESS;
 }
+
+

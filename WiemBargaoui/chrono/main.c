f9a898759dea3528a029ce34a5fc5cecfca26d3c
wiem12 <47714697+wiem12@users.noreply.github.com>
2019-05-07 00:38:30 +0200

    Add files via upload

---
+++ WiemBargaoui/chrono/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#include"SDL/SDL.h"
+#include"SDL/SDL_image.h"
+#include"SDL/SDL_mixer.h"
+#include"chrono.h"
+
+int  main()
+{
+
+
+afficher_chrono();
+
+}
